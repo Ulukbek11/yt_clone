78a793aa68a12add6a338ce7896bd8f8e27d19f2
Ulukbek11 <lucky1hlucky1h@gmail.com>
2026-03-10 13:16:30 +0600

    first commit

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'd63f4ca3f8msh17e6693c98a992bp14bb9djsn3858f4007fe1',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
